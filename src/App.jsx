a25973715810c5fc4ca5847f73dd6ea082abccbd
ayan878 <ayanraza197@gmail.com>
2024-06-01 22:59:15 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,39 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Pricing from "./pages/Pricing";
+import Features from "./pages/Features";
+import Layout from "./components/Layout";
+
+
+// Define the router with the routes
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "home",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "pricing",
+        element: <Pricing />,
+      },
+      {
+        path: "features",
+        element: <Features />,
+      },
+    ],
+  },
+]);
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <RouterProvider router={router}/>
+  );
 }
