f2378de0fedc43623e2b36dafb8c3e9435c8614d
ayan878 <ayanraza197@gmail.com>
2024-06-02 16:25:27 +0530

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,28 @@
 import { Link, NavLink } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
 import logo from "../assets/logo.png";
+import Hamburger from "./Hamburger";
+import { useState } from "react";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <header className="flex justify-between items-center px-24 py-8">
-      <div className="flex justify-between items-center gap-8">
+    <header className="flex justify-between items-center pr-12 py-8">
+      <div className="flex items-center justify-between gap-4">
         <Link to="/">
           <img src={logo} alt="logo" />
         </Link>
-        <nav className="flex justify-between gap-4 ">
-          <NavLink className="font-bold text-red-600" to="/home" activeClassName="active">
+        <nav className="hidden md:flex gap-4">
+          <NavLink
+            className="font-bold text-red-600"
+            to="/home"
+            activeClassName="active"
+          >
             Home
           </NavLink>
           <NavLink to="/about" activeClassName="active">
@@ -23,7 +36,13 @@ function Header() {
           </NavLink>
         </nav>
       </div>
-      <button className="w-24 h-8 rounded-md bg-black text-white">Download</button>
+      <button className="hidden md:block w-24 h-8 rounded-md bg-black text-white">
+        Download
+      </button>
+      <button className="md:hidden" onClick={toggleMenu}>
+        {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+      </button>
+      {isOpen && <Hamburger toggleMenu={toggleMenu} />}
     </header>
   );
 }
