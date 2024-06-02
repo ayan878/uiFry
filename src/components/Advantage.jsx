18dcece469fa9865aebe384aca0585066903327f
ayan878 <ayanraza197@gmail.com>
2024-06-02 16:22:10 +0530

    assets

---
+++ src/components/Advantage.jsx
@@ -0,0 +1,44 @@
+import { BsBell } from "react-icons/bs";
+import advantage from "../assets/features-icons/advatage.png"
+import featureSpot from "../assets/features-icons/featureSpot.png"
+
+
+function Advantage() {
+  return (
+    <div className="flex flex-col md:flex-row items-center gap-8">
+      <div className="md:w-1/2">
+        <p className="text-sm font-extralight text-red-500 uppercase mb-2">
+          advantage
+        </p>
+        <h1 className="text-3xl font-bold mb-2 tracking-tighter capitalize">
+          Why choose uiFry?
+        </h1>
+        <div className="flex items-center space-x-2 mb-4">
+          <BsBell className="w-8 h-8 rounded-full bg-red-500 p-2 text-white" />
+          <h1 className="font-semibold ">Clever Notification</h1>
+        </div>
+        <p className="text-gray-600">
+          Arcu at dictum sapien, mollis. Vulputate sit id accumsan, ultricies.
+          In ultrices malesuada elit mauris etiam odio. Duis tristique lacus, et
+          blandit viverra nisl velit. Sed mattis rhoncus, diam suspendisse sit
+          nunc, gravida eu. Lectus eget eget ac dolor neque lorem sapien,
+          suspendisse aliquam.
+        </p>
+      </div>
+      <div className="md:w-1/2 relative">
+        <img
+          src={featureSpot}
+          alt="feature spot"
+          className="absolute top-0 left-0 w-full h-full z-0"
+        />
+        <img
+          src={advantage}
+          alt="feature"
+          className="w-full h-auto relative z-10"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Advantage;
